6ea4245f259308b2a06b91114bac920d0fca64a8
Robert Tchabo <tchabopats@gmail.com>
2019-02-02 11:48:22 +0000

    Added Postgresql connection

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'rest_framework',
+    'rest_framework',
     'bank'
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PG_NAME','entities'),
+        'USER': os.environ.get('PG_USER','bankapp'),
+        'PASSWORD': os.environ.get('PG_PASS','changeit'),
+        'HOST':     os.environ.get('PG_HOST','localhost'),
+        'PORT':     os.environ.get('PG_PORT','5432')
     }
 }
 
