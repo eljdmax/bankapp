600403811489825e6be28597506e99f928536ba9
robert.tchabo <robert-patrick.tchabo-lekane@hp.com>
2019-07-23 15:33:12 +0100

    moved frontend dir

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+from bank.views import FrontendAppView
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	re_path('api/(?P<version>(v1|v2))/', include('bank.urls'))
+	re_path('api/(?P<version>(v1|v2))/', include('bank.urls')),
+    re_path(r'^', FrontendAppView.as_view()),
 ]
